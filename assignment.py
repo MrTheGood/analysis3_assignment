8585eda14cf4a3c26980197a879d2396edfff506
isti12345 <0953404@hr.nl>
2019-04-26 14:35:51 +0200

    dit is hoe ver ik kwam

--- assignment.py
+++ assignment.py
@@ -17,3 +17,19 @@ class Libarian(Person):
 
 class Author(Person):
     def __init__(self):
+
+
+class Book:
+    def __init__(self,Author,isbn,country,link,pages,title, year,imagelink,language):
+        self.Author = Author
+        self.isbn = isbn
+        self.country = country
+        self.link = link
+        self.pages = pages
+        self.title = title
+        self.year = year
+        self.imagelink = imagelink
+        self.language = language
+
+class BookItem(Book):
+    def __init__(self):
