215a21010d1ee5665b14ae7b0e062de7c259f6d4
isti12345 <0953404@hr.nl>
2019-04-26 14:20:01 +0200

    ik hoop dat het geen onzin is

---
+++ assignment.py
@@ -0,0 +1,19 @@
+class Person:
+    def __init__(self,firstName,lastName):
+        self.firstName = firstName
+        self.lastName = lastName
+
+    def fullName:
+            return firstName + lastName
+
+
+class Costumer(Person):
+    def __init__(self):
+
+
+class Libarian(Person):
+    def __init__(self):
+
+
+class Author(Person):
+    def __init__(self):
